8a48bed65ef46e6477f2b4f59d5084dfa3707860
Oggal <Philip.dan.taylor@gmail.com>
2022-12-19 12:50:56 -0800

    Correct Task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -6,12 +6,12 @@
  *
  * Return: Sibling to node, or NULL
  */
-binary_tree_t *bianary_tree_sibling(binary_tree_t *node)
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
 	if (node->parent->left == node)
-		return (parent->right);
+		return (node->parent->right);
 	else
-		return (parent->left);
+		return (node->parent->left);
 }
