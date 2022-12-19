744c3f8d53d6f8c6452e68b830b63324d18b2031
Oggal <Philip.dan.taylor@gmail.com>
2022-12-19 08:58:09 -0800

    Add Files for task 15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - Find count of nodes
+ * @tree : Tree to check
+ * Return: sum of nodes in children plus one, or 0 if is leaf
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree || ((!tree->left) != (!tree->right)))
+		return (0);
+	if (tree->left)
+		return (binary_tree_is_full(tree->left) ||
+			binary_tree_is_full(tree->right));
+	return (1);
+}
