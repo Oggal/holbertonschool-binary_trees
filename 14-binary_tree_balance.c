3f1855e0b68ee178848fa310df800f166dc59cbd
Oggal <Philip.dan.taylor@gmail.com>
2022-12-19 08:47:48 -0800

    Copy Tree height method into task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,3 +11,24 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	return ((int)binary_tree_height(tree->left) - (int)binary_tree_height(tree->right));
 }
+
+
+
+/**
+ * binary_tree_height - Find height of a tree
+ * @tree : Tree to check
+ * Return: Height of children + 1
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l, r;
+
+	if (!tree || ((!tree->right) && (!tree->left)))
+		return (0);
+	l = binary_tree_height(tree->left);
+	r = binary_tree_height(tree->right);
+
+	if (l < r)
+		l = r;
+	return (l + 1);
+}
