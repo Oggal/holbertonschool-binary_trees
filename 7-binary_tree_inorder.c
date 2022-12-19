b4475de07f9274ad51faf5dd2cec5a0e247f940f
Oggal <Philip.dan.taylor@gmail.com>
2022-12-19 07:58:35 -0800

    Correct task 6 and 7 for null function pointer

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -12,7 +12,8 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	/* 1 */
 	binary_tree_inorder(tree->left, func);
 	/* 2 */
-	func(tree->n);
+	if (func)
+		func(tree->n);
 	/* 3 */
 	binary_tree_inorder(tree->right, func);
 }
