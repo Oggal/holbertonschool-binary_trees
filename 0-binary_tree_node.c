e017aa33eab74ec38baf3489693197a7849e24c1
Oggal <Philip.dan.taylor@gmail.com>
2022-12-19 07:25:20 -0800

    Add Files for Task 1, Correct Task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -6,7 +6,7 @@
  * @parent: parent of new node
  * @value: value of new node
  *
- * Return:
+ * Return: adress of new node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
@@ -16,6 +16,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return (NULL);
 	node->parent = parent;
 	node->n = value;
+	node->right = NULL;
+	node->left = NULL;
 
 	return (node);
 }
