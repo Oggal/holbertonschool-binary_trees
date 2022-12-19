37f12e106533f5947575ec0230bcba7f7cd0b803
Oggal <Philip.dan.taylor@gmail.com>
2022-12-19 09:12:40 -0800

    Correct Logic in return

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,7 +10,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree || ((!tree->left) != (!tree->right)))
 		return (0);
 	if (tree->left)
-		return (binary_tree_is_full(tree->left) ||
+		return (binary_tree_is_full(tree->left) &&
 			binary_tree_is_full(tree->right));
 	return (1);
 }
