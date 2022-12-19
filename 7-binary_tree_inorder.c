7b893bd3b0cd12ac4ad6c375f11bc19fe68bfd8b
Oggal <Philip.dan.taylor@gmail.com>
2022-12-19 07:56:57 -0800

    Correct Function Calls1

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -10,9 +10,9 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (!tree)
 		return;
 	/* 1 */
-	binary_tree_preorder(tree->left, func);
+	binary_tree_inorder(tree->left, func);
 	/* 2 */
 	func(tree->n);
 	/* 3 */
-	binary_tree_preorder(tree->right, func);
+	binary_tree_inorder(tree->right, func);
 }
