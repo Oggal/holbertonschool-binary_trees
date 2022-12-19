cf8ceb6e891343238e2353a379d9b7e9dd74db35
Oggal <Philip.dan.taylor@gmail.com>
2022-12-19 07:28:52 -0800

    Add Files for Task 2, Correction to Task 1

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,13 +2,13 @@
 
 
 /**
- * binary_tree_insert_left - create a new binary tree node
+ * binary_tree_insert_right - create a new binary tree node
  * @parent: parent of new node
  * @value: value of new node
  *
  * Return: node
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
@@ -19,11 +19,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	node->parent = parent;
 	node->n = value;
-	node->left = parent->left;
-	if (node->left)
-		node->left->parent = node;
-	node->right = NULL;
-	parent->left = node;
+	node->right = parent->right;
+	if (node->right)
+		node->right->parent = node;
+	node->left = NULL;
+	parent->right = node;
 
 	return (node);
 }
